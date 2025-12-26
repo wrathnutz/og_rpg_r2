198f439a4cca3413df03ace2ce4c4920b1102e9a
wrathnutz <wrathnut@hotmail.com>
2025-12-26 11:24:23 -0500

    update water reflect
    updated water reflect to automatically update collision shape

---
+++ scenes/entities/objects/water_reflect/water_reflect.gd
@@ -0,0 +1,23 @@
+@tool
+extends ColorRect
+
+@onready var StaticBody : StaticBody2D = $StaticBody2D
+@onready var CollisionShape : CollisionShape2D = $StaticBody2D/CollisionShape2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var old_pos : Vector2 = self.position
+	var new_pos : Vector2 = Vector2(0,0)
+	new_pos.x = old_pos.x + (self.size.x / 2)
+	new_pos.y = old_pos.y + (self.size.y / 2)
+	CollisionShape.shape.set_size(self.size)
+	StaticBody.global_position = new_pos
+	#CollisionShape.shape.set_position(new_pos)
+	print("setting position: " + str(new_pos))
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
