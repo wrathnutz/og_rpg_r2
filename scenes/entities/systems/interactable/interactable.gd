ca16b86dd62985d683839649e528d04c7d6b0f79
wrathnutz <wrathnut@hotmail.com>
2025-12-27 16:34:14 -0500

    Created Interaction System
    added interacting component to the player.
    added interactable component for objects

---
+++ scenes/entities/systems/interactable/interactable.gd
@@ -0,0 +1,7 @@
+extends Area2D
+
+@export var interact_name : String  = ""
+@export var is_interactable : bool = true
+
+var interact: Callable = func():
+	pass
---
+++ scenes/entities/systems/interacting_component/interacting_component.gd
@@ -0,0 +1,42 @@
+extends Node2D
+
+@onready var interact_label: Label = $InteractLabel
+var current_interactions := []
+var can_interact : bool = true
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("action") and can_interact:
+		if current_interactions:
+			can_interact = false
+			interact_label.hide()
+			
+			await current_interactions[0].interact.call()
+			
+			can_interact = true
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	if current_interactions and can_interact:
+		current_interactions.sort_custom(_sort_by_nearest)
+		if current_interactions[0].is_interactable:
+			interact_label.text = current_interactions[0].interact_name
+			interact_label.show()
+	else:
+		interact_label.hide()
+
+
+func _on_interact_range_area_entered(area: Area2D) -> void:
+	current_interactions.push_back(area)
+
+
+func _on_interact_range_area_exited(area: Area2D) -> void:
+	current_interactions.erase(area)
+
+func _sort_by_nearest(area1, area2) -> bool:
+	var area1_dist = global_position.direction_to(area1.global_position)
+	var area2_dist = global_position.direction_to(area2.global_position)
+	return area1_dist < area2_dist
