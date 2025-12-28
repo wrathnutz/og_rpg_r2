1b82fe1889d63170a66d5dae1cec75d11ea36a94
wrathnutz <wrathnut@hotmail.com>
2025-12-28 11:18:42 -0500

    updated input map
    added movement inputs

--- scenes/entities/player/character_body_2d.gd
+++ scenes/entities/player/character_body_2d.gd
@@ -12,11 +12,9 @@ func allow_movement_inputs(allow : bool) -> void:
 	allow_movement = allow
 	animated_sprite.play("idle")
 
-
-
 func _physics_process(_delta: float) -> void:
 	if allow_movement:
-		var direction_h := Input.get_axis("ui_left", "ui_right")
+		var direction_h := Input.get_axis("move_left", "move_right")
 		if direction_h:
 			velocity.x = direction_h * SPEED
 			if direction_h > 0:
@@ -26,7 +24,7 @@ func _physics_process(_delta: float) -> void:
 		else:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
 		
-		var direction_v := Input.get_axis("ui_up", "ui_down")
+		var direction_v := Input.get_axis("move_up", "move_down")
 		if direction_v:
 			velocity.y = direction_v * SPEED
 			if direction_v > 0:
