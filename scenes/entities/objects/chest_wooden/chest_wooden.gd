17dfc3649432eb716d0817277444b9be490b3a55
wrathnutz <wrathnut@hotmail.com>
2025-12-28 16:35:10 -0500

    added wooden chest
    added wooden chest object to game and intro dungeon

---
+++ scenes/entities/objects/chest_wooden/chest_wooden.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+enum chest_state{OPENED=0, CLOSED=1, OPENING=3}
+
+var current_state : chest_state = chest_state.CLOSED
+@export var is_locked : bool = false
+@onready var interactable: Area2D = $Interactable
+@onready var chest_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var sfx_opening: AudioStreamPlayer = $AncientGameWoodenDoorOpen2
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	interactable.interact = try_open
+
+func try_open() -> void:
+	chest_sprite.play("opening")
+	sfx_opening.play()
+	await chest_sprite.animation_finished
+	chest_sprite.play("opened")
+	pass
