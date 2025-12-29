e7e91f625ad7aa1d46e8f800063e017490b0e4b0
wrathnutz <wrathnut@hotmail.com>
2025-12-28 20:40:24 -0500

    updated player creation menu
    added most functionality

--- scenes/menus/character_creation/character_creation.gd
+++ scenes/menus/character_creation/character_creation.gd
@@ -42,3 +42,112 @@ var player_class : String = ""
 func _ready() -> void:
 	name_choice.grab_focus()
 	player_class = class_choice.get_item_text(class_choice.get_selected())
+	_roll_all_stats()
+	_update_mods()
+	_update_screen()
+
+func _input_valid()->bool:
+	var retval : bool = true
+	var errMsg : String = ""
+	if name_choice.text == "":
+		retval = false
+		errMsg += "The Player Name cannot be left blank \n"
+		
+	if !retval:
+		var dialog = ConfirmationDialog.new()
+		dialog.title = "Input Error"
+		dialog.dialog_text = errMsg
+		add_child(dialog)
+		dialog.popup_centered()
+		dialog.show()
+	return retval
+
+func _roll_stat() -> int:
+	
+	var d1 : int = GameState.rng.randi_range(1, 6)
+	var d2 : int = GameState.rng.randi_range(1, 6)
+	var d3 : int = GameState.rng.randi_range(1, 6)
+	
+	return d1 + d2 + d3
+
+func _roll_all_stats() -> void:
+	Strength = _roll_stat()
+	Dexterity = _roll_stat()
+	Constitution = _roll_stat()
+	Intelligence = _roll_stat()
+	Wisdom = _roll_stat()
+	Charisma = _roll_stat()
+
+func _update_mods() -> void:
+	_zero_mods()
+	match class_choice.get_item_text(class_choice.get_selected()):
+		"Warrior":
+			Strength_Mod = 2
+			Constitution_Mod = 1
+			Intelligence_Mod = -2
+			Wisdom_Mod = -1
+		"Wizard":
+			Intelligence_Mod = 2
+			Wisdom_Mod = 1
+			Strength_Mod = -2
+			Constitution_Mod = -1
+		"Thief":
+			Dexterity_Mod = 2
+			Charisma_Mod = 1
+			Constitution_Mod = -2
+			Strength_Mod = -1
+		"Ranger":
+			Dexterity_Mod = 1
+			Constitution_Mod = 1
+			Wisdom_Mod = 1
+			Strength_Mod = -1
+			Intelligence_Mod = -1
+			Charisma_Mod = -1
+
+func _zero_mods()->void:
+	Strength_Mod = 0
+	Dexterity_Mod = 0
+	Constitution_Mod = 0
+	Intelligence_Mod = 0
+	Wisdom_Mod = 0
+	Charisma_Mod = 0
+
+func _update_screen() -> void:
+	#update base stats
+	str_text.text = str(Strength)
+	dex_text.text = str(Dexterity)
+	con_text.text = str(Constitution)
+	int_text.text = str(Intelligence)
+	wis_text.text = str(Wisdom)
+	char_text.text = str(Charisma)
+	
+	#update mods	
+	str_text_mod.text = _get_prefix(Strength_Mod)
+	dex_text_mod.text = _get_prefix(Dexterity_Mod)
+	con_text_mod.text = _get_prefix(Constitution_Mod)
+	int_text_mod.text = _get_prefix(Intelligence_Mod)
+	wis_text_mod.text = _get_prefix(Wisdom_Mod)
+	char_text_mod.text = _get_prefix(Charisma_Mod)
+
+func _get_prefix(value: int)->String:
+	if value >= 0:
+		return "+" + str(value)
+	return str(value)
+
+
+func _on_btn_reroll_pressed() -> void:
+	sfx_dice.play()
+	_roll_all_stats()
+	_update_screen()
+
+
+func _on_btn_save_pressed() -> void:
+	sfx_button.play()
+	if _input_valid():
+		creation_complete.emit()
+		queue_free()
+
+func _on_option_button_item_selected(_index: int) -> void:
+	sfx_button.play()
+	_update_mods()
+	_update_screen()
